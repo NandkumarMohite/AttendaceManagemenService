69bcf46d1b0e40a3afe4cd8ef0b6531acf6a177d
NandkumarMohite <n.b.mohite88@gmail.com>
2024-03-02 17:38:30 +0530

    added sorting logic

--- src/main/java/com/app/service/AttendenceService.java
+++ src/main/java/com/app/service/AttendenceService.java
@@ -119,13 +119,11 @@ public class AttendenceService {
 //		  return list;
 		  List<Attendence> at = u.getAttendence();
 		  System.out.println("sssss-----"+at.size());
-//		  List<AttendenceDto> list = new ArrayList<>();
-//		  list = Arrays.asList(mapper.map(at, AttendenceDto[].class));
 			at.forEach(x->System.out.println(x));
-				List<Attendence> sortedList = at.stream()
-				.sorted(Comparator.comparing(Attendence::getDate))
-				.collect(Collectors.toList());
-		  return sortedList;
+			List<Attendence> sortedList = at.stream()
+			.sorted(Comparator.comparing(Attendence::getDate))
+			.collect(Collectors.toList());
+	  return sortedList;
 	}
 	public Attendence getLastRecordByUserId(int  id) {
         return attendenceDao.findTopByUserIdOrderByidDesc(id);
