ff4f7bf91cc72c0c5c6916372502b616324e05ec
NandkumarMohite <n.b.mohite88@gmail.com>
2024-07-15 16:16:11 +0530

    test

--- src/test/java/com/app/AttendenceApplicationTests.java
+++ src/test/java/com/app/AttendenceApplicationTests.java
@@ -1,14 +1,1 @@
-package com.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AttendenceApplicationTests {
-
-	@Test
-	void contextLoads() {
-		
-	}
-
-}
